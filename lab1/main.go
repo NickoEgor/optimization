57788debd84c9bc0d0dadd615f5ddd028ce89e84
NickoEgor <egor1998nick@gmail.com>
2019-02-13 22:34:19 +0300

    Create inversing method in lab 1

--- lab1/main.go
+++ lab1/main.go
@@ -4,11 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"gonum.org/v1/gonum/mat"
-	// "math/rand"
 	"os"
 	"strconv"
 	"strings"
-	// "time"
 )
 
 func matPrint(X mat.Matrix) {
@@ -18,30 +16,6 @@ func matPrint(X mat.Matrix) {
 }
 
 func getKnownMatrices() (*mat.Dense, *mat.Dense) {
-	// s := rand.NewSource(time.Now().Unix())
-	// r := rand.New(s)
-	// length := 3
-	// vl := make([]float64, length*length)
-	// m1 := mat.NewDense(length, length, vl)
-	// m2 := mat.NewDense(length, length, nil)
-	// is_acceptable := false
-
-	// for !is_acceptable {
-	// for i := 0; i < length*length; i++ {
-	// vl[i] = float64((r.Int() % 7) - 4)
-	// }
-
-	// if mat.Det(m1) == 1 {
-	// is_acceptable = true
-	// }
-	// }
-
-	// m2.Inverse(m1)
-	// matPrint(m1)
-	// matPrint(m2)
-
-	// return m1, m2
-
 	v1 := []float64{
 		7, 2, 1,
 		0, 3, -1,
@@ -89,13 +63,7 @@ func checkInverse(m1, m2 mat.Matrix) bool {
 	return true
 }
 
-func main() {
-	A, A_i := getKnownMatrices()
-	println("Source matrix:")
-	matPrint(A)
-	println("Inversed matrix:")
-	matPrint(A_i)
-
+func method(A, A_i mat.Matrix, vec *mat.VecDense, index int) *mat.Dense {
 	res := checkInverse(A, A_i)
 	if !res {
 		println("Matrices are incorrect")
@@ -103,15 +71,37 @@ func main() {
 	}
 
 	r, _ := A.Dims()
-
-	vec := getReplacingVector()
-	println("Replacing vector:")
-	matPrint(vec)
-
 	if vec.Len() != r {
 		println("Incorrect vector size")
 		os.Exit(1)
 	}
+
+	l := mat.NewVecDense(r, nil)
+	l.MulVec(A_i, vec)
+	li := l.AtVec(index)
+	if li == 0 {
+		println("l[i] equals to 0")
+		os.Exit(0)
+	}
+
+	l.SetVec(index, -1)
+	l.ScaleVec(-1.0/li, l)
+
+	Q := eye(r)
+	Q.SetCol(index, l.RawVector().Data)
+	return
+}
+
+func main() {
+	A, A_i := getKnownMatrices()
+	println("Source matrix:")
+	matPrint(A)
+	println("Inversed matrix:")
+	matPrint(A_i)
+
+	vec := getReplacingVector()
+	println("Replacing vector:")
+	matPrint(vec)
 
 	print("Enter column index: ")
 	reader := bufio.NewReader(os.Stdin)
@@ -124,29 +114,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	A.SetCol(index, vec.RawVector().Data)
-	println("Inversion needed for matrix:")
-	matPrint(A)
-
-	l := mat.NewVecDense(r, nil)
-	l.MulVec(A_i, vec)
-
-	li := l.AtVec(index)
-	if li == 0 {
-		println("l[i] equals to 0")
-		os.Exit(0)
-	}
-
-	l.SetVec(index, -1)
-	l.ScaleVec(-1.0/li, l)
-	Q := eye(r)
-	Q.SetCol(index, l.RawVector().Data)
-	Ans := mat.NewDense(r, r, nil)
-	Ans.Product(Q, A_i)
-	println("New inversed matrix:")
-	matPrint(Ans)
-
-	println("Product:")
-	A.Product(A, Ans)
-	matPrint(A)
+	ans := method(A, A_i, vec, index)
+	matPrint(ans)
 }
